8845153cf8b57a994039fbe8a24854e56898c258
samarendra12 <kumarsamarendra1@gmail.com>
2025-08-10 00:52:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,25 +1,28 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.38.1"
-    }
-  }
+ required_providers {
+   azurerm = {
+   source = "hashicorp/azurerm"
+   version = "4.38.1"
+   }
+}
+backend "azurerm" {
+resource_group_name = "sagar"
+storage_account_name = "420420"
+container_name = "gautam"
+key = "terraform.tfstate"
+}
 
-  backend "azurerm" {
-    resource_group_name  = "sagar"
-    storage_account_name = "420420"  
-    container_name       = "gautam"
-    key                  = "terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
-  features {}
-  subscription_id = "bcf304d8-21b8-4358-9c77-54b5dd26f938"
-}
-
+    features {}
+        
+      subscription_id= "bcf304d8-21b8-4358-9c77-54b5dd26f938"
+        
+      }
+    
 resource "azurerm_resource_group" "group_rp" {
-  name     = "pipeline_rg"
-  location = "east us"
+    name = "pipeline_rg"
+    location = "east us"
+  
 }
