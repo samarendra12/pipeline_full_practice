a0c14f7ae861b679e686a7a89402c9579f2d599d
samarendra12 <kumarsamarendra1@gmail.com>
2025-08-24 23:16:09 +0530

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "Parash" {
-    name = "samarstg1"
+    name = "samarstg2"
     location = "central india"
-    resource_group_name = "ajay_rg"
+    resource_group_name = "ajay_rg1"
     account_tier = "Standard"
     account_replication_type = "GRS"
 
